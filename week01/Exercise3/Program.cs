f146109af9305177da87ea5695cfd05fd4e0f82b
ekfrempong-devops <ranemmfrempz@gmail.com>
2026-09-07 20:52:32 +0000

    Added if, else if statements and applied break keyword to finish assignment.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,40 @@ using System;
 class Program
 {
     static void Main(string[] args)
+   
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+    
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+        
+        while (magicNumber != -1)
+        {
+        Console.Write ("What is your guess? ");
+        string guessedNum = Console.ReadLine();
+        int number = int.Parse(guessedNum);
+        
+
+        if (number < magicNumber) 
+            {
+                Console.WriteLine ("Higher");
+            }
+        else if (number > magicNumber)
+            {
+                Console.WriteLine ("Lower");
+            }
+        else
+            {
+                Console.WriteLine ("You guessed it!");
+                break;
+            }
+       
+
+        }
+
+
+
     }
+
+
+        
 }
