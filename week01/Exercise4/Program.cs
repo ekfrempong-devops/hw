9ce325297926adcf8f7a565f778ada236825720d
ekfrempong-devops <ranemmfrempz@gmail.com>
2026-09-07 23:02:48 +0000

    Added conditional statements and loops

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Net.Quic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int num = -1;
+
+    while (num != 0)
+    {
+        Console.Write("Enter a list of numbers, (0 to quit):");
+        string response = Console.ReadLine();
+        int numb = int.Parse(response);
+        
+
+    if (numb != 0)
+        {
+            numbers.Add(numb);
+        }
+
+    int sum = 0;
+    foreach (int number in numbers)
+        {
+            sum += numb;
+        }
+
+    Console.WriteLine($"The sum is {sum}");
+
+    float average = ((float) sum) / numbers.Count;
+    Console.WriteLine($"The average is {average}");
+
+    int max = numbers [0];
+
+    foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+            Console.WriteLine($"The max is {max}");
+        }
+
     }
+
+        
+    }        
+
+
+
+        
+
 }
