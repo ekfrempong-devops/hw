e7eb3c04cc9f22583eacee3f99fe591ca7420690
ekfrempong-devops <ranemmfrempz@gmail.com>
2026-09-08 23:44:05 +0000

    Edited the code to have a single else, else if and else statement. Added a separate if/else statement at the end of the code to display to user whether they passed or not. Added a single print statement that prints the letter grade once.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
@@ -9,94 +10,80 @@ class Program
 
         int percentageNumber = int.Parse(gradePercentage);
 
+        string letter = "";
+        string sign = "";
+        
+        
         if (percentageNumber >= 97)
         {
-           string letter = "A";
-           string sign = "+";
-           Console.WriteLine ($"{letter}{sign}");
-            
+           letter = "A";
+           sign = "+";   
         }
         else if (percentageNumber >= 90 && percentageNumber < 93)
         {
-            string letter = "A";
-            string sign = "-";
-            Console.WriteLine ($"{letter}{sign}");
-
+            letter = "A";
+            sign = "-";
         }
         else if (percentageNumber == 93 || percentageNumber == 94 || percentageNumber == 95 || percentageNumber == 96) 
         {
-            string letter = "A";
-            Console.WriteLine ($"{letter}");
-        
+            letter = "A";
         }
 
 
         else if (percentageNumber >= 87 || percentageNumber == 89)
         {
-            string letter = "B";
-            string sign = "+";
-            Console.WriteLine ($"{letter}{sign}");
-            
+            letter = "B";
+            sign = "+";   
         }
         else if (percentageNumber >= 80 && percentageNumber < 83)
         {
-            string letter = "B";
-            string sign = "-";
-            Console.WriteLine ($"{letter}{sign}");
-    
+            letter = "B";
+            sign = "-";
         }
         else if (percentageNumber == 83 || percentageNumber == 84 || percentageNumber == 85 || percentageNumber == 86)
         {
-            string letter = "B";
-            Console.WriteLine ($"{letter}");
+            letter = "B";  
         }
 
-    
+
         else if (percentageNumber >= 77 && percentageNumber <=79)
         {
-            string letter = "C";
-            string sign = "+";
-            Console.WriteLine ($"{letter}{sign}");
-            
+            letter = "C";
+            sign = "+";    
         }
         else if (percentageNumber >= 70 && percentageNumber < 73)
         {
-            string letter = "C";
-            string sign = "-";
-            Console.WriteLine ($"{letter}{sign}");
-            
+            letter = "C";
+            sign = "-";   
         }
         else if (percentageNumber == 73 || percentageNumber == 74 || percentageNumber == 75 || percentageNumber == 76)
         {
-            string letter = "C";
-            Console.WriteLine ($"{letter}");
+            letter = "C";     
         }
+
         
         else if (percentageNumber >= 67 && percentageNumber <=69)
         {
-            string letter = "D";
-            string sign = "+";
-            Console.WriteLine ($"{letter}{sign}");
-        
+            letter = "D";
+            sign = "+";
         }
         else if (percentageNumber >= 60 && percentageNumber < 63)
         {
-            string letter = "D";
-            string sign = "-";
-            Console.WriteLine ($"{letter}{sign}");
-        
+            letter = "D";
+            sign = "-";
         }
         else if (percentageNumber == 63 || percentageNumber == 64 || percentageNumber == 65 || percentageNumber == 66)
         {
-            string letter = "D";
-            Console.WriteLine ($"{letter}");
+            letter = "D";   
         }
+        
 
         else if (percentageNumber < 60)
         {
-            string letter = "F";
-            Console.WriteLine ($"{letter}");
+            letter = "F";
         }
+
+        Console.WriteLine ($"Your grade is {letter}{sign}");
 
         if (percentageNumber >= 70) 
         {
@@ -106,7 +93,9 @@ class Program
         else
         {
             Console.WriteLine("You failed to pass. Better luck next time! ");
-        }    
+        }  
+
+              
         
     }
 }
