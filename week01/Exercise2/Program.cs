7cf679f6ddc23063e76675fa256eabf3d8568285
ekfrempong-devops <ranemmfrempz@gmail.com>
2026-09-08 22:42:04 +0000

    Added a separate if/else statement to display to user whether they passed or not.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -14,36 +14,36 @@ class Program
            string letter = "A";
            string sign = "+";
            Console.WriteLine ($"{letter}{sign}");
-           Console.WriteLine ("Congratulations! You passed the course."); 
+            
         }
         else if (percentageNumber >= 90 && percentageNumber < 93)
         {
             string letter = "A";
             string sign = "-";
             Console.WriteLine ($"{letter}{sign}");
-            Console.WriteLine ("Congratulations! You passed the course.");
+
         }
         else if (percentageNumber == 93 || percentageNumber == 94 || percentageNumber == 95 || percentageNumber == 96) 
         {
             string letter = "A";
             Console.WriteLine ($"{letter}");
-            Console.WriteLine ("Congratulations! You passed the course.");
+        
         }
 
 
-        if (percentageNumber >= 87 || percentageNumber == 89)
+        else if (percentageNumber >= 87 || percentageNumber == 89)
         {
             string letter = "B";
             string sign = "+";
             Console.WriteLine ($"{letter}{sign}");
-            Console.WriteLine ("Congratulations! You passed the course.");
+            
         }
         else if (percentageNumber >= 80 && percentageNumber < 83)
         {
             string letter = "B";
             string sign = "-";
             Console.WriteLine ($"{letter}{sign}");
-            Console.WriteLine ("Congratulations! You passed the course.");
+    
         }
         else if (percentageNumber == 83 || percentageNumber == 84 || percentageNumber == 85 || percentageNumber == 86)
         {
@@ -52,19 +52,19 @@ class Program
         }
 
     
-        if (percentageNumber >= 77 && percentageNumber <=79)
+        else if (percentageNumber >= 77 && percentageNumber <=79)
         {
             string letter = "C";
             string sign = "+";
             Console.WriteLine ($"{letter}{sign}");
-            Console.WriteLine ("Congratulations! You passed the course.");
+            
         }
         else if (percentageNumber >= 70 && percentageNumber < 73)
         {
             string letter = "C";
             string sign = "-";
             Console.WriteLine ($"{letter}{sign}");
-            Console.WriteLine ("Congratulations! You passed the course.");
+            
         }
         else if (percentageNumber == 73 || percentageNumber == 74 || percentageNumber == 75 || percentageNumber == 76)
         {
@@ -72,19 +72,19 @@ class Program
             Console.WriteLine ($"{letter}");
         }
         
-        if (percentageNumber >= 67 && percentageNumber <=69)
+        else if (percentageNumber >= 67 && percentageNumber <=69)
         {
             string letter = "D";
             string sign = "+";
             Console.WriteLine ($"{letter}{sign}");
-            Console.WriteLine ("Unfortunately, you did not pass the course. Better luck next time.");
+        
         }
         else if (percentageNumber >= 60 && percentageNumber < 63)
         {
             string letter = "D";
             string sign = "-";
             Console.WriteLine ($"{letter}{sign}");
-            Console.WriteLine ("Unfortunately, you did not pass the course. Better luck next time.");
+        
         }
         else if (percentageNumber == 63 || percentageNumber == 64 || percentageNumber == 65 || percentageNumber == 66)
         {
@@ -92,16 +92,21 @@ class Program
             Console.WriteLine ($"{letter}");
         }
 
-        if (percentageNumber < 60)
+        else if (percentageNumber < 60)
         {
             string letter = "F";
             Console.WriteLine ($"{letter}");
-            Console.WriteLine ($"You got an {letter}. You failed the course. You need to retake it!");
         }
 
+        if (percentageNumber >= 70) 
+        {
+            Console.WriteLine("Congratulations...You passed!");
+        }
 
-        
-         
+        else
+        {
+            Console.WriteLine("You failed to pass. Better luck next time! ");
+        }    
         
     }
 }
