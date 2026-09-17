6c743eba5b058f55cd5b0c83b98fcd849a1c6905
ekfrempong-devops <ranemmfrempz@gmail.com>
2026-09-17 11:01:30 +0000

    Completed activity for W03

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
-    }
+        Fraction _fraction1 = new Fraction(1,1);
+        Console.WriteLine(_fraction1.GetFractionString());
+        Console.WriteLine(_fraction1.GetDecimalValue());
+        
+        Fraction _fraction2 = new Fraction(5);
+        Console.WriteLine(_fraction2.GetFractionString());
+        Console.WriteLine(_fraction2.GetDecimalValue());
+
+        Fraction _fraction3 = new Fraction(3,4);
+        Console.WriteLine(_fraction3.GetFractionString());
+        Console.WriteLine(_fraction3.GetDecimalValue());
+
+        Fraction _fraction4 = new Fraction(1,3);
+        Console.WriteLine(_fraction4.GetFractionString());
+        Console.WriteLine(_fraction4.GetDecimalValue());
+    }   
 }
